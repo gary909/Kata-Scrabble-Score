a7accdfdab7cf6070ca85e634f3e47ef14cafaac
gary <gary.white@lodgit.com>
2022-07-06 13:39:04 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function scrabbleScore(str) {
+    // ...
+}
+
+console.log(scrabbleScore("")); // 0
+console.log(scrabbleScore('a')); // 1
+console.log(scrabbleScore('street')); // 6
+console.log(scrabbleScore(' a')); // 1
+console.log(scrabbleScore("st re et")); // 6
